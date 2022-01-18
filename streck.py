334e9b4e920ccf0edc63af482dac52ba239162f7
andeha <andeha@users.noreply.github.com>
2022-01-18 23:46:36 +0100

    Improved exposition.

---
+++ streck.py
@@ -0,0 +1,9 @@
+import sublime, sublime_plugin, datetime
+
+class InstantCommand(sublime_plugin.TextCommand):
+   def run(self, edit):
+      first_is_caret_sel = self.view.sel() # ⬷ a 'set' of regions (first,last) points.
+      timestamp = datetime.datetime.now()
+      text = timestamp.strftime("%Y-%m-%d %H:%M:%S ")
+      self.view.insert(edit,first_is_caret_sel[0].begin(),text)
+
