72b4ede4e0f1c6d32f3ca7ca446922cd9a0bb978
andeha <andeha@users.noreply.github.com>
2022-01-26 18:39:54 +0100

    Improved exposition.

--- table-and-streck.py
+++ table-and-streck.py
@@ -2,15 +2,15 @@
 
 import sublime, sublime_plugin, datetime
 
-def Instant():
-   timestamp = datetime.datetime.now()
-   return timestamp.strftime("%Y-%m-%d %H:%M:%S ")
-
 class InstantCommand(sublime_plugin.TextCommand):
    def run(self, edit):
      text = Instant()
      first_is_caret_sel = self.view.sel() # ⬷ a 'set' of regions (first,last) points.
      self.view.insert(edit,first_is_caret_sel[0].begin(),text)
+
+def Instant():
+   timestamp = datetime.datetime.now()
+   return timestamp.strftime("%Y-%m-%d %H:%M:%S ")
 
 class TurbinCompletionsEventListener(sublime_plugin.EventListener):
    def on_query_completions(self, view, prefix, locations):
@@ -29,12 +29,55 @@ class TurbinCompletionsEventListener(sublime_plugin.EventListener):
             sublime.KIND_SNIPPET
           ),
           sublime.CompletionItem(
-            "func", 
-            annotation="(FUNCTION DEFINED)", 
-            completion="FUNCTION ${1:name} ($2)\n{\n $0\n}", 
-            completion_format=sublime.COMPLETION_FORMAT_SNIPPET, 
-            kind=sublime.KIND_SNIPPET
+            "func", "(FUNCTION DEFINED)", 
+            "FUNCTION ${1:name} ($2)\n{\n $0\n}", 
+            sublime.COMPLETION_FORMAT_SNIPPET, 
+            sublime.KIND_SNIPPET
+          ),
+          sublime.CompletionItem(
+            "caccount", "(ACCOUNT CREATED)", 
+            Instant() + " CREATE ACCOUNT A1920 WITH-ENTITY Company1 NAMED 'Kassa och bank'\n",
+            sublime.COMPLETION_FORMAT_SNIPPET,
+            sublime.KIND_SNIPPET
+          ),
+          sublime.CompletionItem(
+            "ccurrency", "(CURRENCY CREATED)", 
+            Instant() + " CREATE CURRENCY USD NAMED 'US Dollar'\n",
+            sublime.COMPLETION_FORMAT_SNIPPET,
+            sublime.KIND_SNIPPET
+          ),
+          sublime.CompletionItem(
+            "centity", "(ENTITY CREATED)", 
+            Instant() + " CREATE ENTITY ${1:identity} NAMED '${2:name}' TRADING IN USD RESIDENT IN USA\n",
+            sublime.COMPLETION_FORMAT_SNIPPET,
+            sublime.KIND_SNIPPET
+          ),
+          sublime.CompletionItem(
+            "bookkeep", "(RECORDED)", 
+            Instant() + " BOOKKEEP Company1 {\n"
+            "  DEBET A1920 WITH 10.00\n"
+            "  CREDIT A2641 WITH 9.00\n"
+            "  CREDIT A2440 WITH 1.00\n"
+            "} COMMENT 'Verificate 1'\n",
+            sublime.COMPLETION_FORMAT_SNIPPET,
+            sublime.KIND_SNIPPET
+          ),
+          sublime.CompletionItem(
+            "schedule", "(NOTIFICATION)", 
+            Instant() + " CREATE SCHEDULE DaytimeWork\n" 
+            " STARTING 2022-01-18 21:13:16\n"
+            " OCCURING BI-HOURLY\n"
+            " /* ENDING 2022-01-18 21:13:26 /*\n"
+            "  {\n"
+            "    VAR sales = 100.00 + Normal(100.00, 25.00)\n"
+            "    VAR usdAmount = Convert(sales,EUR,USD)\n"
+            "    BOOKKEEP Company1 {\n"
+            "    DEBET A1920 WITH 10.00 + usdAmount\n"
+            "      CREDIT Company1[VATOUT] WITH 9.00 + usdAmount\n"
+            "      CREDIT A2440 WITH 1.00 + usdAmount\n"
+            "    } COMMENT 'Verificate ' + INSTANCE\n"
+            "  }",
+            sublime.COMPLETION_FORMAT_SNIPPET,
+            sublime.KIND_SNIPPET
           ),
        ], 0)
-
-
